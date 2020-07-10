ce7ab42fc6dc7fd3c673f97384e087f5169e1248
Louis Albert Apas <louisalbertapas25@gmail.com>
2020-07-10 14:35:09 +0800

    Add publisher.cpp

---
+++ publisher.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <zmq.h>
+#include <cassert>
+#include <unistd.h>
+
+int main()
+{
+    std::cout << "ZMQ Publiser using libzmq" << std::endl;
+
+    void *ctx = zmq_ctx_new(); // create context
+    void *pub = zmq_socket(ctx, ZMQ_PUB); // create a socket that publishes
+    int rc = zmq_bind(pub, "tcp://*:5570"); // binds the rep tcp socket to an address and to port 5570
+    assert(rc == 0); // bind should return non zero
+
+    for(;;)
+    {
+        std::string str;
+        str = "msg" + std::to_string(rand() % 10000); // publish msg0 up to msg9999
+        std::cout << "Publishing " << str << std::endl;
+        zmq_send(pub, str.c_str(), str.length(), 0);
+        sleep(2);
+    }
+
+    return 0;
+}
